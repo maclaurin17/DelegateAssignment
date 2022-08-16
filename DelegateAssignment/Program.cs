29b0e41d62b590ca4d4ffb3464a336a692cb26bb
Support <Support@DESKTOP-16NLN76>
2022-08-16 09:03:18 +0100

    Add project files.

---
+++ DelegateAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DelegateAssignment
+{/// <summary>
+/// This program describes a delegate called DelegateName
+/// </summary>
+    internal class Program
+    {
+        public delegate string DelegateName(string firstName, string lastName);
+        static void Main(string[] args)
+        {
+            Del x = new Del();
+            var likelyUsername = x.FirstName + " " + x.LastName;
+            
+
+            Delegate pred = (Del x) =>
+            {
+                if (x.FirstName == "James" && x.LastName == "Samson")
+                {
+                    return true;
+                }
+                else return false;
+            };
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+
+
+        public class Del
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string UserName { get; set; }    
+        }
+    }
+
